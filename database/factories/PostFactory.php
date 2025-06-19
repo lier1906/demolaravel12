5dbaa8f630cc8063fd7c64bd254db8449e93dc3f
LIER.JIMENEZ <lier.jimenez@ucr.ac.cr>
2025-06-18 18:45:02 -0600

    Se corrigieron los errores para que funcionara el LikesTest

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+   public function definition()
+{
+    return [
+        'title' => $this->faker->sentence,
+        // agrega más si es necesario según tu tabla de posts
+    ];
+}
+
+}
